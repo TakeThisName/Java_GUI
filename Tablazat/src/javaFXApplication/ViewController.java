43e8cac07d5ec197201d89f443130bd46622a858
avaaris <avaaris@gmail.com>
2018-10-17 19:58:03 +0200

    4

--- Tablazat/src/javaFXApplication/ViewController.java
+++ Tablazat/src/javaFXApplication/ViewController.java
@@ -3,6 +3,8 @@ package javaFXApplication;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -14,16 +16,16 @@ import javafx.scene.control.TextField;
 public class ViewController implements Initializable{
 
     @FXML
-    private TableView<?> tabla;
+    private TableView<Szemely> tabla;
 
     @FXML
-    private TableColumn<?, ?> tabla_ID;
+    private TableColumn<Szemely, Number> tabla_ID;
 
     @FXML
-    private TableColumn<?, ?> Tabla_Vezeteknev;
+    private TableColumn<Szemely, String> Tabla_Vezeteknev;
 
     @FXML
-    private TableColumn<?, ?> Tabla_Keresztnev;
+    private TableColumn<Szemely, String> Tabla_Keresztnev;
 
     @FXML
     private TextField TextF_Keresztnev;
@@ -44,7 +46,29 @@ public class ViewController implements Initializable{
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    	adat_Hozzaad();
+    	tablazat_Letrehoz();
+    }
+    
+    private ObservableList<Szemely> adat = FXCollections.observableArrayList();
+    
+    private void adat_Hozzaad() {
+    	Szemely szemely1 = new Szemely(1, "Izé", "Izéke");
+    	Szemely szemely2 = new Szemely(2, "Dodó", "Kacsa");
+    	Szemely szemely3 = new Szemely(3, "B", "B");
     	
+    	adat.add(szemely1);
+    	adat.add(szemely2);
+    	adat.add(szemely3);
+    }
+    
+    private void tablazat_Letrehoz() {
+    	tabla_ID.setCellValueFactory(cellaAdat -> cellaAdat.getValue().idProperty()); // case sensitivity !!!
+    	Tabla_Vezeteknev.setCellValueFactory(cellaAdat -> cellaAdat.getValue().vezeteknevProperty());
+    	Tabla_Keresztnev.setCellValueFactory(cellaAdat -> cellaAdat.getValue().keresztnevProperty());
+    	tabla.setItems(adat);
+    	
+    	// tablazatVezeteknev.setCellValueFactory(new Cellback<TableColumn.CellDatra)
     }
 
 }
