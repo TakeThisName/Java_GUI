04150eadd90bff7f9a7b780dd00e23ce50a526e3
avaaris <avaaris@gmail.com>
2018-10-17 18:59:45 +0200

    Set_Get_R

--- Tablazat/src/javaFXApplication/Szemely.java
+++ Tablazat/src/javaFXApplication/Szemely.java
@@ -1,5 +1,6 @@
 package javaFXApplication;
 
+import javafx.beans.property.IntegerPropertyBase;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
@@ -9,5 +10,36 @@ public class Szemely {
 	private SimpleStringProperty vezeteknev;
 	private SimpleStringProperty keresztnev;
 	
+	public final String getVezeteknev(String vezeteknev) {
+		return this.vezeteknev.get();
+	}
 	
+	public final void setVezeteknev(String vezeteknev) {
+		this.vezeteknevProperty().set(vezeteknev);
+	}
+
+	public final SimpleStringProperty vezeteknevProperty() {
+		return this.vezeteknev;
+	}
+	
+	
+	public final String getKeresztnev(String keresztnev) {
+		return this.keresztnev.get();
+	}
+	
+	public final void setKeresztnev(String keresztnev) {
+		this.keresztnevProperty().set(keresztnev);
+	}
+	
+	public final SimpleStringProperty keresztnevProperty() {
+		return this.keresztnev;
+	}
+
+	public SimpleIntegerProperty getId() {
+		return id;
+	}
+
+	public void setId(SimpleIntegerProperty id) {
+		this.id = id;
+	}
 }
